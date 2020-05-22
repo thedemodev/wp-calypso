1d5dc1d1cc122a63c0927863b98f7bd095b7e6d2
Dale du Preez <dale@automattic.com>
2020-05-22 16:53:32 +0200

    Separate out refund policy for new domain registrations and renewals

--- client/my-sites/checkout/checkout/redirect-payment-box.jsx
+++ client/my-sites/checkout/checkout/redirect-payment-box.jsx
@@ -26,7 +26,7 @@ import CountrySpecificPaymentFields from './country-specific-payment-fields';
 import { isWpComBusinessPlan, isWpComEcommercePlan } from 'lib/plans';
 import { validatePaymentDetails, maskField, unmaskField } from 'lib/checkout';
 import { PAYMENT_PROCESSOR_COUNTRIES_FIELDS } from 'lib/checkout/constants';
-import DomainRegistrationRefundPolicy from './domain-registration-refund-policy';
+import DomainRefundPolicy from './domain-refund-policy';
 import DomainRegistrationAgreement from './domain-registration-agreement';
 
 export class RedirectPaymentBox extends PureComponent {
@@ -333,7 +333,7 @@ export class RedirectPaymentBox extends PureComponent {
 					<TermsOfService
 						hasRenewableSubscription={ hasRenewableSubscription( this.props.cart ) }
 					/>
-					<DomainRegistrationRefundPolicy cart={ this.props.cart } />
+					<DomainRefundPolicy cart={ this.props.cart } />
 					<DomainRegistrationAgreement cart={ this.props.cart } />
 
 					<div className="checkout__payment-box-actions">
