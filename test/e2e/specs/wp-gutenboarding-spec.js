155d6abe5fe4ce535a9fb364cc9a61b731a23ada
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-06-05 18:50:06 +0300

    Update Gutenboarding flow e2e tests: - remove vertical sub-step - update selectors - add Plans step

--- test/e2e/specs/wp-gutenboarding-spec.js
+++ test/e2e/specs/wp-gutenboarding-spec.js
@@ -12,6 +12,7 @@ import NewPage from '../lib/pages/gutenboarding/new-page.js';
 import AcquireIntentPage from '../lib/pages/gutenboarding/acquire-intent-page.js';
 import DesignSelectorPage from '../lib/pages/gutenboarding/design-selector-page.js';
 import StylePreviewPage from '../lib/pages/gutenboarding/style-preview-page.js';
+import PlansPage from '../lib/pages/gutenboarding/plans-page.js';
 
 import * as driverManager from '../lib/driver-manager.js';
 import * as dataHelper from '../lib/data-helper.js';
@@ -30,7 +31,7 @@ before( async function () {
 describe( 'Gutenboarding: (' + screenSize + ') @parallel', function () {
 	this.timeout( mochaTimeOut );
 	describe( 'Visit Gutenboarding page as a new user', function () {
-		const vertical = dataHelper.randomPhrase();
+		//const vertical = dataHelper.randomPhrase();
 		const siteTitle = dataHelper.randomPhrase();
 
 		before( async function () {
@@ -45,7 +46,8 @@ describe( 'Gutenboarding: (' + screenSize + ') @parallel', function () {
 
 		step( 'Can see Acquire Intent and set a vertical and site title', async function () {
 			const acquireIntentPage = await AcquireIntentPage.Expect( driver );
-			await acquireIntentPage.enterVertical( vertical );
+			//sub-step temporary removed
+			//await acquireIntentPage.enterVertical( vertical );
 			await acquireIntentPage.enterSiteTitle( siteTitle );
 			return await acquireIntentPage.goToNextStep();
 		} );
@@ -58,9 +60,13 @@ describe( 'Gutenboarding: (' + screenSize + ') @parallel', function () {
 			);
 		} );
 
-		step( 'Can see Style Preview and continue to site creation', async function () {
+		step( 'Can see Style Preview and continue', async function () {
 			const stylePreviewPage = await StylePreviewPage.Expect( driver );
-			return await stylePreviewPage.continueToCreateSite();
+			return await stylePreviewPage.continue();
+		} );
+
+		step( 'Can see Plans Grid', async function () {
+			return await PlansPage.Expect( driver );
 		} );
 	} );
 
